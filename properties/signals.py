2e68e97d3478ed4703d0e834f6c88f1bfb74baf5
ELIUZ <osongoeliuz14@gmail.com>
2025-09-03 14:33:06 +0300

    Invalidate the all_properties Redis cache on Property create/update/delete using Django signals

---
+++ properties/signals.py
@@ -0,0 +1,19 @@
+# properties/signals.py
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.core.cache import cache
+from .models import Property
+
+@receiver(post_save, sender=Property)
+def invalidate_cache_on_save(sender, instance, **kwargs):
+    """
+    Clear cached properties when a Property is created/updated.
+    """
+    cache.delete('all_properties')
+
+@receiver(post_delete, sender=Property)
+def invalidate_cache_on_delete(sender, instance, **kwargs):
+    """
+    Clear cached properties when a Property is deleted.
+    """
+    cache.delete('all_properties')
